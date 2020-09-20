6dd93d309b457128f69de65aa38cd6f4edfe01d8
Jefferson Melo <jefferson@localhost.localdomain>
2020-09-20 10:46:38 -0300

    first commit

---
+++ scripts/conversao-jpg-png.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/arquivos/learning/shellscript-begin/imagens-livros/
+
+if [ ! -d png ]
+then
+	mkdir png
+fi
+
+for imagem in *.jpg
+do
+	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1}')
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png 
+done
+

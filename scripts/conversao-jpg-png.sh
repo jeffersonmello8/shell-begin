8771f7cdb615bb97c86f143d36e677a6c97327c0
Jefferson Melo <jefferson.mello8@hotmail.com>
2021-01-20 22:25:01 -0300

    i added comments and updated the image directory

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd ~/arquivos/learning/shellscript-begin/imagens-livros/
+cd ~/arquivos/shell-begin/imagens-livros/
 
+#verify if the folder png already exists
 if [ ! -d png ]
 then
 	mkdir png
 fi
 
+#converts all images the folder imagens-livros
 for imagem in *.jpg
 do
 	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1}')

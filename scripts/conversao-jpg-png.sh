8483bc2c97a7199a2ebeb5827657e1bccca00cac
Jefferson Melo <jefferson.mello8@hotmail.com>
2021-01-21 07:33:27 -0300

    created function and if to show errors messages redirecting to a log file

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+converts_images(){
 cd ~/arquivos/shell-begin/imagens-livros/
-
-#verify if the folder png already exists
+#verify if the png folder already exists
 if [ ! -d png ]
 then
 	mkdir png
@@ -14,4 +14,13 @@ do
 	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1}')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png 
 done
+}
+
+converts_images 2>log.txt
+if [ $? -eq 0 ]
+then 
+	echo "Conversion performed successfully"
+else
+	echo "Conversion failed"
+fi
 
